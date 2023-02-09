8bf9c3bf8ce22483056aa38fabe61c5c5b727d7c
Chilly-Cheese <rubymdecker01@gmail.com>
2023-02-09 13:36:22 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,142 @@
+import pygame as pg
+import sys
+from os import path
+from settings import *
+from sprites import *
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT), pg.SCALED, vsync=1)
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        pg.key.set_repeat(500, 100)
+        global levelMaps
+        global mapIndex
+        levelMaps = ['map1.txt', 'map2.txt', 'map3.txt']
+        mapIndex = 0
+        self.load_map()
+        self.background = pg.Surface(self.screen.get_size())
+        self.background = pg.transform.scale(self.background, self.screen.get_size())
+        for row in range(GRIDHEIGHT):
+            for col in range(GRIDWIDTH):
+                self.background.blit(pg.image.load("FloorTile.png"), (col * TILESIZE, row * TILESIZE))
+
+    def load_map(self):
+        global levelMaps
+        global mapIndex
+        game_folder = path.dirname(__file__)
+        self.map_data = []
+        with open(path.join(game_folder, levelMaps[mapIndex]), 'rt') as f:
+            for line in f:
+                self.map_data.append(line)
+        if mapIndex < (len(levelMaps)-1):
+            mapIndex += 1
+        else:
+            mapIndex = 0
+
+    def new(self):
+        # initialize all variables and do all the setup for a new game
+        self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+        self.exits = pg.sprite.Group()
+        self.hazards = pg.sprite.Group()
+        self.bluePortals = pg.sprite.Group()
+        self.orangePortals = pg.sprite.Group()
+        self.boxes = pg.sprite.Group()
+        for row, tiles in enumerate(self.map_data):
+            for col, tile in enumerate(tiles):
+                if tile == '1':
+                    Wall(self, col, row)
+                if tile == 'P':
+                    self.player = Player(self, col, row)
+                if tile == 'E':
+                    Exit(self, col, row)
+                if tile == 'X':
+                    Lava(self, col, row)
+                if tile == 'O':
+                    self.orangePortal = PortalOrange(self, col, row)
+                if tile == 'B':
+                    self.bluePortal = PortalBlue(self, col, row)
+                if tile == '@':
+                    Box(self, col, row)
+                    
+    def cleanup(self):
+        for sprite in self.all_sprites:
+            sprite.kill()
+
+    def run(self):
+        # game loop - set self.playing = False to end the game
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000
+            self.events()
+            self.update()
+            self.draw()
+
+    def quit(self):
+        pg.quit()
+        sys.exit()
+
+    def update(self):
+        # update portion of the game loop
+        self.all_sprites.update()
+        
+        if self.player.atExit:
+            self.cleanup()
+            self.load_map()
+            self.new()
+        if self.player.dead:
+            self.cleanup()
+            self.new()
+        if self.player.touchingBlue:
+            self.player.y = self.orangePortal.y
+            self.player.x = self.orangePortal.x + 1
+            self.player.touchingBlue = False
+        if self.player.touchingOrange:
+            self.player.y = self.bluePortal.y
+            self.player.x = self.bluePortal.x + 1
+            self.player.touchingOrange = False
+        
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+
+    def draw(self):
+        self.screen.blit(self.background, (0,0))
+        self.draw_grid()
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
+
+    def events(self):
+        # catch all events here
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.quit()
+                if event.key == pg.K_LEFT:
+                    self.player.move(dx=-1)
+                if event.key == pg.K_RIGHT:
+                    self.player.move(dx=1)
+                if event.key == pg.K_UP:
+                    self.player.move(dy=-1)
+                if event.key == pg.K_DOWN:
+                    self.player.move(dy=1)
+
+    def show_start_screen(self):
+        pass
+
+    def show_go_screen(self):
+        pass
+
+# create the game object
+g = Game()
+g.show_start_screen()
+while True:
+    g.new()
+    g.run()
+    g.show_go_screen()
---
+++ sprites.py
@@ -0,0 +1,185 @@
+import pygame as pg
+from settings import *
+
+class Player(pg.sprite.Sprite):
+    global collidedBox
+    
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.atExit = False
+        self.dead = False
+        self.touchingBlue = False
+        self.touchingOrange = False
+
+    def move(self, dx=0, dy=0):
+        global collidedBox
+        if self.collide_with_hazard(dx, dy):
+            self.dead = True
+        if self.collide_with_exit(dx, dy):
+            self.atExit = True
+        if self.collide_with_portal_blue(dx, dy):
+            self.touchingBlue = True
+        if self.collide_with_portal_orange(dx, dy):
+            self.touchingOrange = True
+        if self.collide_with_box(dx, dy):
+            collidedBox.move(dx, dy)
+        if not self.collide_with_walls(dx, dy):
+            if not self.collide_with_immovable_box(dx, dy):
+                self.x += dx
+                self.y += dy
+        
+
+    def collide_with_walls(self, dx=0, dy=0):
+        for wall in self.game.walls:
+            if wall.x == self.x + dx and wall.y == self.y + dy:
+                return True
+        return False
+    
+    def collide_with_exit(self, dx=0, dy=0):
+        for exit in self.game.exits:
+            if exit.x == self.x + dx and exit.y == self.y + dy:
+                return True
+        return False
+    
+    def collide_with_hazard(self, dx=0, dy=0):
+        for hazard in self.game.hazards:
+            if hazard.x == self.x + dx and hazard.y == self.y + dy:
+                return True
+        return False
+    
+    def collide_with_portal_blue(self, dx=0, dy=0):
+        for portal in self.game.bluePortals:
+            if portal.x == self.x + dx and portal.y == self.y + dy:
+                return True
+        return False
+    def collide_with_portal_orange(self, dx=0, dy=0):
+        for portal in self.game.orangePortals:
+            if portal.x == self.x + dx and portal.y == self.y + dy:
+                return True
+        return False
+
+    def collide_with_box(self, dx=0, dy=0):
+        global collidedBox
+        for box in self.game.boxes:
+            if box.x == self.x + dx and box.y == self.y + dy:
+                collidedBox = box
+                return True
+        return False
+
+    def collide_with_immovable_box(self, dx=0, dy=0):
+        for box in self.game.boxes:
+            if box.x == self.x + dx and box.y == self.y + dy:
+                return True
+        return False
+        
+    def update(self):
+        self.rect.x = self.x * TILESIZE
+        self.rect.y = self.y * TILESIZE
+
+class Wall(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.walls
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
+
+class Exit(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.exits
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(BLUE)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
+        
+class Lava(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.hazards
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
+        
+class PortalOrange(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.orangePortals
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(ORANGE)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
+
+class PortalBlue(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.bluePortals
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(CYAN)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
+        
+class Box(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.boxes
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(BOX)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
+        self.immovable = False
+    
+    def collide_with_walls(self, dx=0, dy=0):
+        for wall in self.game.walls:
+            if wall.x == self.x + dx and wall.y == self.y + dy:
+                return True
+        return False
+    
+    def collide_with_box(self, dx=0, dy=0):
+        for box in self.game.boxes:
+            if box.x == self.x + dx and box.y == self.y + dy:
+                return True
+        return False
+    
+    def move(self, dx=0, dy=0):
+        if not self.collide_with_walls(dx, dy):
+            if not self.collide_with_box(dx, dy):
+                self.x += dx
+                self.y += dy
+        
+    def update(self):
+        self.rect.x = self.x * TILESIZE
+        self.rect.y = self.y * TILESIZE
